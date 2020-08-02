8fc91354ebaa96980e41134afe5329af6d92e1e2
synerzip <skothari@digarc.com>
2020-08-02 23:01:38 +0530

    initial commit

---
+++ version.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package   plugintype_pluginname
+ * @copyright 2020, You Name <your@email.address>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 20200208;
+$plugin->requires = 2020060900;
+//$plugin->supported = [37, 38];   // Available as of Moodle 3.9.0 or later.
+//$plugin->incompatible = 36;   // Available as of Moodle 3.9.0 or later.
+$plugin->component = 'tool_shitalkothari';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0';
+
+//$plugin->dependencies = [
+//    'mod_forum' => ANY_VERSION,
+//    'mod_data' => TODO
+//];
+
